9a5ef6d694e54fdfa2bbec1450ace58f9605b118
Lise Feliers <lise.feliers@etu.minesparis.psl.eu>
2025-07-03 10:44:07 +0200

    conv

--- conversion_pdf-markdown.py
+++ conversion_pdf-markdown.py
@@ -5,7 +5,7 @@ from markitdown import MarkItDown
 # Conversion pdf en Markdown
 
 markitdown = MarkItDown()
-result = markitdown.convert("pdfs/WAX_0.pdf")
+result = markitdown.convert("pdfs\WAX_0.pdf")
 output_file = "WAX_0.md"
 
 with open(output_file, "w", encoding="utf-8") as file:
